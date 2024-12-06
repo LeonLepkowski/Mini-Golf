e5132b0698e76474a459eb987117ca16802ddef3
Leon <lelek0801@gmail.com>
2024-12-06 16:58:53 +0100

    Add Button Animation & Merge Scripts

--- Assets/Scripts/Mill.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Mill : MonoBehaviour
-{
-    void Update()
-    {
-        transform.Rotate(0f, 15f * Time.deltaTime, 0f, Space.Self);
-    }
-}
--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -2,8 +2,32 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+    public float bounceAmplitude = 0.2f;
+    public float bounceFrequency = 1f;
+
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
     void Update()
     {
-        transform.Rotate(0f, 30f * Time.deltaTime, 0f, Space.Self);
+        if (gameObject.CompareTag("Button"))
+        {
+            transform.Rotate(0f, 60f * Time.deltaTime, 0f, Space.Self);
+
+            float newY = initialPosition.y + Mathf.Sin(Time.time * bounceFrequency) * bounceAmplitude;
+            transform.position = new Vector3(initialPosition.x, newY, initialPosition.z);
+        }
+        else if (gameObject.CompareTag("Wings"))
+        {
+            transform.Rotate(0f, 15f * Time.deltaTime, 0f, Space.Self);
+        }
+        else if (gameObject.CompareTag("Rotation"))
+        {
+            transform.Rotate(0f, 30f * Time.deltaTime, 0f, Space.Self);
+        }
     }
 }
